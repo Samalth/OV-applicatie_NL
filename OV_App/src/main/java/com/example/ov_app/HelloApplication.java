e2671c7f8848433758ad93a27947a91b94fa73d0
NeedForProgramming <141028245+NeedForProgramming@users.noreply.github.com>
2023-11-16 11:41:53 +0100

    Hoi Sam

--- OV_App/src/main/java/com/example/ov_app/HelloApplication.java
+++ OV_App/src/main/java/com/example/ov_app/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
-        stage.setTitle("Hello!");
+        stage.setTitle("Hello Sam!");
         stage.setScene(scene);
         stage.show();
     }
@@ -21,3 +21,4 @@ public class HelloApplication extends Application {
         launch();
     }
 }
+
