1583463a5f900be98d9dc07118a10767e0176511
Sam Alex <sam.thiers@student.hu.nl>
2023-11-21 09:38:03 +0100

    1.0.1

--- OV_App/src/main/java/com/ovapp/Application.java
+++ OV_App/src/main/java/com/ovapp/Application.java
@@ -6,11 +6,11 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class Application extends javafx.application.Application {
+public class   Application extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("OVapp_GUI.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 960, 540);
+        Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
         stage.setTitle("OV App");
         stage.setScene(scene);
         stage.show();
--- OV_App/src/main/java/com/ovapp/Controller.java
+++ OV_App/src/main/java/com/ovapp/Controller.java
@@ -1,6 +1,8 @@
 package com.ovapp;
-
+import java.util.*;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 
 public class Controller {
@@ -8,7 +10,48 @@ public class Controller {
     private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Voorbeeld");
+    protected void onGOClick() {
+        welcomeText.setText("Just kidding, I like girls!");
+    }
+
+    @FXML
+    private Button GO; {onGOClick();}
+
+    @FXML
+    private ComboBox<String> vertrekComboBox;
+
+    @FXML
+    private ComboBox<String> aankomstComboBox;
+
+    @FXML
+    private ComboBox<String> vertrekStadComboBox;
+
+    @FXML
+    private ComboBox<String> aankomstStadComboBox;
+
+
+    public void initialize() {
+        // Vul de ChoiceBox met tijden
+        List<String> tijden = getTijden();
+        vertrekComboBox.getItems().addAll(tijden);
+        aankomstComboBox.getItems().addAll(tijden);
+
+        List<String> steden = getSteden();
+        vertrekStadComboBox.getItems().addAll(steden);
+        aankomstStadComboBox.getItems().addAll(steden);
+    }
+
+    private List<String> getTijden() {
+        List<String> tijden = new ArrayList<>();
+        for (int uur = 0; uur <= 23; uur++) {
+            for (int minuut = 0; minuut <= 59; minuut += 15) {
+                tijden.add(String.format("%02d:%02d", uur, minuut));
+            }
+        }
+        return tijden;
+    }
+
+    private List<String> getSteden() {
+        return Arrays.asList("Amersfoort", "Nieuwegein", "Amsterdam", "Den Haag", "Den Bosch", "Arnhem", "Utrecht", "IJsselstein");
     }
 }
