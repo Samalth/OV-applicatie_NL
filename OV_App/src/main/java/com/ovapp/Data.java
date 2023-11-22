8273054fc01b73ab3ab42793288598a2efb815fa
Devaki Jonker <D@>
2023-11-22 19:42:49 +0100

    External file

--- OV_App/src/main/java/com/ovapp/Data.java
+++ OV_App/src/main/java/com/ovapp/Data.java
@@ -1,10 +1,10 @@
 package com.ovapp;
 
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
 public class Data {
 	public static ObservableList<String> departureData() {
-		return FXCollections.observableArrayList(
-				"Option 1", "Option 2", "Option 3");
+		return FXCollections.observableArrayList("Option 1", "Option 2", "Option 3");
 	}
 }
