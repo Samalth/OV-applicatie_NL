2c5467ce0fda4341f34e53e720192c5269af8b85
D. Jonker <d.jonker@>
2023-11-22 15:08:30 +0100

    0.3 Populated vertrek choicebox

---
+++ OV_App/src/main/java/com/ovapp/Data.java
@@ -0,0 +1,10 @@
+package com.ovapp;
+
+import javafx.collections.ObservableList;
+
+public class Data {
+	public static ObservableList<String> departureData() {
+		return FXCollections.observableArrayList(
+				"Option 1", "Option 2", "Option 3");
+	}
+}
