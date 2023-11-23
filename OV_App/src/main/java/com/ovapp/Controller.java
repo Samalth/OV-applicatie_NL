a663a59dcfa7bf962ffe4f2ec529128531f28a2f
Max Wijnker <max.wijnker@student.hu.nl>
2023-11-23 14:05:51 +0100

    Fourth Commit & push by Max, 23-11-2023. Changes: -Added working current digital clock -Added working current date display -Added the function to the "Plan route" button to display the previously chosen locations.

--- OV_App/src/main/java/com/ovapp/Controller.java
+++ OV_App/src/main/java/com/ovapp/Controller.java
@@ -4,28 +4,66 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.*;
 import java.util.*;
+import java.text.*;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.application.Platform;
+
 
 public class Controller {
     @FXML
-    private Label welcomeText;
-
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Voorbeeld");
-    }
-
-    @FXML
     private ComboBox<String> vertrekComboBox;
-
     @FXML
     private ComboBox<String> bestemmingComboBox;
+    @FXML
+    private String Vertrek;
+    @FXML
+    private String Bestemming;
+    @FXML
+    private Label routeLabel;
+    @FXML
+    private Label datumLabel;
+    @FXML
+    private Label klokLabel;
+    @FXML
+    protected void onPlanButtonClick() {
+        Vertrek = vertrekComboBox.getValue();
+        Bestemming = bestemmingComboBox.getValue();
+        routeLabel.setText(String.format("De reis van %s naar %s", Vertrek, Bestemming));
+    }
 
     public void initialize() {
 
         List<String> steden = getSteden();
         vertrekComboBox.getItems().addAll(steden);
         bestemmingComboBox.getItems().addAll(steden);
+
+        Timer timer = new Timer(true);
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                updateKlok();
+                updateDatum();
+            }
+        },0,1000);
     }
+    private void updateKlok() {
+        SimpleDateFormat dateFormat = new SimpleDateFormat(" HH:mm:ss");
+        String formattedTime = dateFormat.format(new Date());
+
+        Platform.runLater(() -> {
+            klokLabel.setText(formattedTime);
+        });
+    }
+    private void updateDatum() {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+        String formattedDate = dateFormat.format(new Date());
+
+        Platform.runLater(() -> {
+            datumLabel.setText(formattedDate);
+        });
+    }
+
     private List<String> getTijden() {
         List<String> tijden = new ArrayList<>();
         for (int uur = 0; uur <= 23; uur++) {
@@ -39,4 +77,6 @@ public class Controller {
     private List<String> getSteden() {
         return Arrays.asList("Amersfoort", "Nieuwegein", "Amsterdam", "Den Haag", "Den Bosch", "Arnhem", "Utrecht", "IJsselstein");
     }
+
+
 }
